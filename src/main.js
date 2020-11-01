ae31f85b63e5bcd375a09398b020a1e3bfa96b34
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-01 13:34:45 -0300

    fix: Concertando bug do v-icon com material icons

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import store from './store'
 import vuetify from '@/plugins/vuetify'
 
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
-
 import axios from 'axios';
 
 // Vue.use(Vuetify, {
