6c28504e044b0669cc29d707c35942fc954c2624
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-01 20:23:08 -0300

    new: Campos de atributos

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -10,10 +10,10 @@ export default {
                     value: 'name',
                 },
                 { text: 'Idade', value: 'age' },
-                { text: 'Armas', value: 'weapons', sortable: false},
-                { text: 'Atributo', value: 'keyAttribute', sortable: false},
-                { text: 'Ataque', value: 'atack'},
-                { text: 'Experiência', value: 'experience'}
+                { text: 'Armas', value: 'weapons', sortable: false },
+                { text: 'Atributo', value: 'keyAttribute', sortable: false },
+                { text: 'Ataque', value: 'atack' },
+                { text: 'Experiência', value: 'experience' }
             ],
             dictAttribute: {
                 strength: 'Força',
@@ -50,11 +50,33 @@ export default {
                 age--;
             }
 
-            return age < 0? 0 : age ;
+            return age < 0 ? 0 : age;
         },
-        geExperience(item) {
+        getExperience(item) {
             const exp = Math.floor((this.getAge(item.birthday) - 7) * Math.pow(22, 1.45))
-            return exp < 0? 0 : exp;
+            return exp < 0 ? 0 : exp;
+        },
+        getAtack(item) {
+            const equippedWeapon = item.weapons.find((element) => { return element.equipped });
+            const keyAttr = item.attributes[item.keyAttribute];
+            const attack = 10 + this.modAttribute(keyAttr) + equippedWeapon.mod
+            return attack;
+        },
+        modAttribute(value) {
+            if (value >= 0 && value <= 8) {
+                return -2;
+            } else if (value > 8 && value <= 10) {
+                return -1;
+            } else if (value > 10 && value <= 12) {
+                return 0;
+            } else if (value > 12 && value <= 15) {
+                return 1;
+            } else if (value > 15 && value <= 18) {
+                return 2
+            } else if (value > 18 && value <= 20) {
+                return 3
+            }
         }
+
     },
 }
--- src/views/knightsForm/KnightsForm.js
+++ src/views/knightsForm/KnightsForm.js
@@ -1,4 +1,4 @@
-import { required } from 'vuelidate/lib/validators';
+import { required, minValue, maxValue } from 'vuelidate/lib/validators';
 
 export default {
     name: 'KnightsForm',
@@ -7,8 +7,25 @@ export default {
         return {
             readonly: "Apenas leitura",
             loading: false,
-            form: {},
+            form: {
+                attributes: {
+                    strength: 0,
+                    dexterity: 0,
+                    constitution: 0,
+                    intelligence: 0,
+                    wisdom: 0,
+                    charisma: 0,
+                },
+            },
             menu: false,
+            attributesList: [
+                { name: "Força", value: "strength" },
+                { name: "Destreza", value: "dexterity" },
+                { name: "Constituição", value: "constitution" },
+                { name: "Inteligência", value: "intelligence" },
+                { name: "Sabedoria", value: "wisdom" },
+                { name: "Carisma", value: "charisma" }
+            ]
 
         }
     },
@@ -18,25 +35,34 @@ export default {
             form: {
                 name: { required },
                 nickname: { required },
-                birthday: { required }
+                birthday: { required },
+                keyAttribute: { required },
+                attributes: {
+                    strength: { maxValue: maxValue(20), minValue: minValue(0) },
+                    dexterity: { maxValue: maxValue(20), minValue: minValue(0) },
+                    constitution: { maxValue: maxValue(20), minValue: minValue(0) },
+                    intelligence: { maxValue: maxValue(20), minValue: minValue(0) },
+                    wisdom: { maxValue: maxValue(20), minValue: minValue(0) },
+                    charisma: { maxValue: maxValue(20), minValue: minValue(0) },
+                }
             },
         };
 
     },
-    created () {
-        if(this.id) {            
-            this.$store.dispatch('getRead', { url: `/knights/${this.id}`})
-            .then(() => {
-                this.form = this.makeFormData();
-            })
+    created() {
+        if (this.id) {
+            this.$store.dispatch('getRead', { url: `/knights/${this.id}` })
+                .then(() => {
+                    this.form = this.makeFormData();
+                })
         }
     },
     methods: {
         makeFormData() {
-            return {...this.instance}
+            return { ...this.instance }
         },
         makeDataPost() {
-            return {...this.form}
+            return { ...this.form }
         },
         saveForm() {
             try {
@@ -45,7 +71,7 @@ export default {
                     if (this.id) {
                         this.edit();
                     } else {
-                        this.create();                  
+                        this.create();
                     }
                 }
             } catch (e) {
@@ -53,7 +79,7 @@ export default {
             }
         },
         goToList() {
-            this.$router.push({name: 'Home'});
+            this.$router.push({ name: 'Home' });
         },
         create() {
             const data = this.makeDataPost();
@@ -70,10 +96,10 @@ export default {
                 });
         },
         remove() {
-            this.$store.dispatch('deleteRecord', { url: `/knights/${this.id}`})
-            .then(() => {
-                this.goToList();
-            })
+            this.$store.dispatch('deleteRecord', { url: `/knights/${this.id}` })
+                .then(() => {
+                    this.goToList();
+                })
         }
     },
     computed: {
@@ -98,5 +124,56 @@ export default {
             !this.$v.form.birthday.required && errors.push('Escreva um nascimento.')
             return errors
         },
+        keyAttributeErrors() {
+            const errors = []
+            if (!this.$v.form.keyAttribute.$dirty) return errors
+            !this.$v.form.keyAttribute.required && errors.push('Escolha a habilidade.')
+            return errors
+        },
+        attributeStrengthErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.strength.$dirty) return errors
+            !this.$v.form.attributes.strength.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.strength.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        attributeDexterityErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.dexterity.$dirty) return errors
+            !this.$v.form.attributes.dexterity.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.dexterity.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        attributeConstitutionErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.constitution.$dirty) return errors
+            !this.$v.form.attributes.constitution.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.constitution.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        attributeIntelligenceErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.intelligence.$dirty) return errors
+            !this.$v.form.attributes.intelligence.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.intelligence.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        attributeWisdomErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.wisdom.$dirty) return errors
+            !this.$v.form.attributes.wisdom.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.wisdom.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        attributeCharismaErrors() {
+            const errors = []
+            if (!this.$v.form.attributes.charisma.$dirty) return errors
+            !this.$v.form.attributes.charisma.maxValue && errors.push('Valor inválido.')
+            !this.$v.form.attributes.charisma.minValue && errors.push('Valor inválido.')
+            return errors
+        },
+        
+        
+        
     },
 }
