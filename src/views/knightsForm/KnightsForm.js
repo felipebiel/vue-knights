cfa6ece15a3bb9f2dd95726e8541e722cc631ca5
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-01 14:59:29 -0300

    new: Campos iniciais dee teste de forumlário

--- src/views/knightsForm/KnightsForm.js
+++ src/views/knightsForm/KnightsForm.js
@@ -1,3 +1,18 @@
 export default {
     name: 'KnightsForm',
+    props: ['id'],
+    data() {
+        return {
+            readonly: "Apenas leitura",
+            loading: false,
+            form : {},
+            menu: false,
+
+        }
+    },
+    computed: {
+        nameErrors() {
+            return []
+        }
+    },
 }
