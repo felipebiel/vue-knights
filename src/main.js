edac7059a8b46c6d9ed34c2d912dc996af067204
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-01 10:15:07 -0300

    new: Instalação do Vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
+Vue.use(vuetify, {
+  iconfont: 'md'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
