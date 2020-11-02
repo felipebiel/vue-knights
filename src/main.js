4130d8bc0eb4e56a4276ac19acbcaf94dc07ae88
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-02 10:31:51 -0300

    new: instalação do vue-sweetalert2

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import vuetify from '@/plugins/vuetify'
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate)
 
+import VueSweetalert2 from 'vue-sweetalert2';
+ 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+ 
+Vue.use(VueSweetalert2);
+
 import axios from 'axios';
 
 Vue.config.productionTip = false
