8d569263a10a1418799604f65e36f9bf03528b5c
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-02 12:04:00 -0300

    fix: Ajuste visual e link do hall of herroes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,26 @@
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+        return {
+            drawer: false,
+            group: null,
+        }
+    },
+    methods: {
+        goToHome() {
+            if (this.$router.history.current.name !== 'home') {
+                this.$router.push({ name: 'home' });
+            }
+        },
+        goToHall() {
+            if (this.$router.history.current.name !== 'hall-of-heroes') {
+                this.$router.push({ name: 'hall-of-heroes' });
+            }
+        }
+    },
+    watch: {
+        group() {
+            this.drawer = false
+        },
+    },
 }
